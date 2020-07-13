49a8982ada4497ae601f53cad98c3d291526b601
Turzhanskyi <turzhansky81@gmail.com>
2020-07-13 21:26:22 +0300

    Add show and inex pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,19 +1,41 @@
 # frozen_string_literal: true
 
 class PicsController < ApplicationController
-  def index; end
+  before_action :find_pic, only: %i[show edit update destroy]
+
+  def index
+    @pics = Pic.all.order('created_at DESC')
+  end
 
   def new
     @pic = Pic.new
   end
 
+  def show; end
+
+  def edit; end
+
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: 'Yesss! It was posted!'
+    else
+      render 'new'
+    end
   end
+
+  def update; end
+
+  def destroy; end
 
   private
 
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 end
