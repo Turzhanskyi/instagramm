f192e4047c18f32487026a484f3abaac536b173c
Turzhanskyi <turzhansky81@gmail.com>
2020-07-18 19:18:05 +0300

    Add uploading pictures

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,4 +2,14 @@
 
 class Pic < ApplicationRecord
   belongs_to :user
+  has_one_attached :pic_image
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless pic_image.attached?
+
+    errors.add(:pic_image, 'is too big') unless pic_image.byte_size <= 1.megabyte
+    acceptable_types = %w[image/jpeg image/png]
+    errors.add(:pic_image, 'must be a JPEG or PNG') unless acceptable_types.include?(pic_image.content_type)
+  end
 end
